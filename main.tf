05e5c3eb7b8b4f6cae8827a3797647bf9cd66be2
Venkat3699 <Ravindrareddy.mukka9@gmail.com>
2025-04-03 21:19:06 +0530

    updated Pipeline

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = "~> 1.5.6"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.93.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "ap-south-1"
 }
