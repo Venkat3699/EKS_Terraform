d2966adc67895157c3431b5bfd8e1e4ec0597473
Venkat3699 <Ravindrareddy.mukka9@gmail.com>
2025-04-03 22:41:21 +0530

    updated tfapply_tfdestroy

--- main.tf
+++ main.tf
@@ -6,7 +6,16 @@ terraform {
       version = "5.93.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "terraform-s3-bucket-1234"    # Replace with your S3 bucket name
+    key            = "terraform.tfstate"        # Path to store the state file
+    region         = "ap-south-1"                     # AWS region
+    encrypt        = true                             # Encrypt state file
+    dynamodb_table = "terraform-state-lock"          # DynamoDB table for state locking
+  }
 }
+
 
 provider "aws" {
   region = "ap-south-1"
